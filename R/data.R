c3260db0c20f26931608a438d1b6267b4427b2a7
mguevara <miguel.guevara@upla.cl>
2020-01-20 15:10:01 -0300

    added second chilean network

--- R/data.R
+++ R/data.R
@@ -1,13 +1,12 @@
 #' ches0611 dataset
 #'
-#' Graph of Chilean Higher Education Space between 20016-2011.
+#' Graph of Chilean Higher Education Space between 2006-2011.
 #'
 #' @format A graph (iGraph object or list) with this configuration:
 #' \describe{
 #' \item{Vertices}{Career program. It includes 10 oder features used in the orginal paper.}
 #' \item{Edges}{Binary link between programs.}
 #' }
-#' @source \url{http://pantheon.media.mit.edu/}
 #' @references 
 #' Candia, C., Encarnação, S., & Pinheiro, F. L. (2019). The higher education space: Connecting degree programs from individuals’ choices. EPJ Data Science, 8(1), 1–17. https://doi.org/10.1140/epjds/s13688-019-0218-4
 #' 
@@ -17,3 +16,22 @@
 #' summary(ches0611)
 #' plot_hes(ches0611)
 "ches0611"
+
+#' ches1217 dataset
+#'
+#' Graph of Chilean Higher Education Space between 2012-2017.
+#'
+#' @format A graph (iGraph object or list) with this configuration:
+#' \describe{
+#' \item{Vertices}{Career program. It includes 10 oder features used in the orginal paper.}
+#' \item{Edges}{Binary link between programs.}
+#' }
+#' @references 
+#' Candia, C., Encarnação, S., & Pinheiro, F. L. (2019). The higher education space: Connecting degree programs from individuals’ choices. EPJ Data Science, 8(1), 1–17. https://doi.org/10.1140/epjds/s13688-019-0218-4
+#' 
+#' @keywords dataset
+#' @examples
+#' data(ches1217)
+#' summary(ches1217)
+#' plot_hes(ches1217)
+"ches1217"
